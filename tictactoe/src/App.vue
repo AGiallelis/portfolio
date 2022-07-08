e9e018cba0f2ecf50b9e56f9aa7a8db340b88854
Antonios Giallelis <antonios.giallelis@factset.com>
2022-07-08 18:29:12 +0300

    cell display & add the winner message to be displayed

--- tictactoe/src/App.vue
+++ tictactoe/src/App.vue
@@ -48,13 +48,31 @@ const ResetGame = () => {
 }
 
 }
-
 </script>
 
 <template>
   <main class="pt-8 text-center dark:bg-gray-800 min-h-screen dark:text-white">
       <h1 class="mb-8 text-3x1 font-bold uppercase">Tic Tac Toe</h1>
       <h3 class="text-xl mb-4">Player {{ player }}'s turn</h3>
+        <div class="flex flex-col items-center mb-8">
+          <div
+              v-for="(row,x) in board"
+              :key="x"
+              class="flex">
+
+            <div 
+                v-for="(cell,y) in row"
+                :key="y"
+                @click="MakeMove(x,y)"
+                :class="`border border-white w-20 h-20 hover:bg-gray-700 
+                flex items-center justify-center material-icons-outlined 
+                text-4x1 cursor pointer`">
+                {{ cell=== 'X' ? `close` : cell === 'O' ? 'circle': ''}}
+            </div>
+          </div>
+        </div>
+      <h2 v-if="winner" class="text-6xl fond-bold mb-8">Player '{{winner}}' wins!</h2>
+      <button @click="ResetGame" class="px-4 py-2 bg-pink-500 rounded uppercase font-bold hover:big-pink-600 duration-300">Reset Game</button>
   </main>
 </template>
 
